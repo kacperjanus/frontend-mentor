af2a370bdc2bac8cd50fbfb1e6f2450607afa72e
kacperjanus <kacperjanus8@gmail.com>
2024-05-16 12:43:49 +0200

    Ajusted spacing for mobile version

--- newsletter-sign-up-form/src/components/Thanks.tsx
+++ newsletter-sign-up-form/src/components/Thanks.tsx
@@ -13,11 +13,13 @@ function Thanks({email, setSuccess, setEmail}: ThanksProps) {
         setSuccess(false);
     }
     return (
-        <div className="div h-[28rem] w-[28rem] bg-white rounded-3xl flex justify-center">
-            <div className="flex flex-col py-10 px-12 gap-7 justify-center">
+        <div className="div sm:h-[28rem] h-screen w-[28rem] bg-white sm:rounded-3xl flex justify-center">
+            <div className="flex flex-col justify-between py-10 px-12 gap-7 sm:justify-center mt-40 sm:mt-0">
+                <div className="flex flex-col gap-7">
                 <img className="w-14 mb-2" src="/images/icon-success.svg"/>
                 <h1 className="text-darkslategray font-bold text-5xl">Thanks for subscribing!</h1>
                 <p className="text-sm">A confirmation email has been sent to <span className="font-bold">{email}</span>. Please open it and click the button inside to confirm your subscription.</p>
+                </div>
                 <Button onClick={handleClick}>Dismiss message</Button>
             </div>
         </div>
