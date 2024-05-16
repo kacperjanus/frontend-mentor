ab85658bf260ac75e861ae003552c019c4c3679b
kacperjanus <kacperjanus8@gmail.com>
2024-05-16 09:22:41 +0200

    Added button glow to hover state

--- newsletter-sign-up-form/src/components/Button.tsx
+++ newsletter-sign-up-form/src/components/Button.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 interface ButtonProps {
     children: string
+    onClick?: (event: React.MouseEvent) => void
 }
 
-function Button({children} : ButtonProps) {
-    //TODO add button glow in active state
+function Button({children, onClick} : ButtonProps) {
     return (
         <div>
-            <button className="bg-darkslategray hover:bg-tomato text-white px-2 py-4 rounded-lg font-semibold w-full"
+            <button onClick={onClick} className="bg-darkslategray hover:bg-gradient-to-r from-[#FF5379] to-tomato hover:shadow-2xl hover:shadow-tomato/50 text-white px-2 py-4 rounded-lg font-semibold w-full"
                     type="submit">{children}
             </button>
         </div>
