27500d4bd1b2a7cdfa3343860b328b3cd4ef0466
kacperjanus <kacperjanus8@gmail.com>
2024-05-25 12:05:03 +0200

    Created static version of Step 4

---
+++ multi-step-form/app/_components/steps/StepFour.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from "@/app/_components/Button";
+
+function StepFour() {
+    return (
+        <div className="flex flex-col mt-4 h-full justify-between">
+            <div className="flex flex-col gap-8">
+                <div className="flex flex-col justify-between bg-magnolia rounded-lg p-5 divide-y divide-light-gray">
+                    <div className="flex justify-between items-center pb-6">
+                        <div>
+                            <p className="text-marine-blue font-bold">Arcade (Monthly)</p>
+                            <a className="text-cool-gray underline">Change</a>
+                        </div>
+                            <p className="text-marine-blue font-bold">$9/mo</p>
+                    </div>
+                    <div className="flex flex-col gap-4 pt-6">
+                        <div className="flex justify-between items-center">
+                            <p className="text-cool-gray">Online service</p>
+                            <p className="text-marine-blue">+$1/mo</p>
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <p className="text-cool-gray">Larger service</p>
+                            <p className="text-marine-blue">+$2/mo</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-between px-6">
+                    <p className="text-cool-gray">Total (per month)</p>
+                    <p className="text-xl text-purplish-blue font-bold">+$12/mo</p>
+                </div>
+            </div>
+            <div className="flex justify-between items-center">
+                <div>
+                    <p className="text-cool-gray">Go Back</p>
+                </div>
+                <div className="mb-4">
+
+                    <Button confirm={true} content="Confirm"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default StepFour;
