d89c5c40b41efbd1b27d26680d0550e8282e5c57
kacperjanus <kacperjanus8@gmail.com>
2024-05-26 16:12:24 +0200

    Added checking if input fields have been filled

--- multi-step-form/app/_components/Input.tsx
+++ multi-step-form/app/_components/Input.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {ChangeEventHandler} from 'react';
+import ansiRegex from "ansi-regex";
 
-function Input({label, placeholder}: {label: string; placeholder: string}) {
+function Input({label, placeholder, value, onChange, isSubmitted}: {label: string; placeholder: string, value: string, onChange: React.Dispatch<React.SetStateAction<string>>, isSubmitted: boolean}) {
+    const handleChange: ChangeEventHandler<HTMLInputElement> = function(e){
+        onChange(e?.target.value)
+    }
+
     return (
         <div className="flex flex-col pt-5">
-            <label className="text-marine-blue">{label}</label>
-            <input required className="p-3 border-solid border-light-gray border-[1px] rounded-xl active:border-marine-blue text-marine-blue font-bold placeholder:font-normal cursor-pointer" placeholder={placeholder}/>
+            <div className="flex justify-between items-center">
+                <label className="text-marine-blue">{label}</label>
+                {isSubmitted && value==="" ? <p className="font-bold text-strawberry-red"> This field is required</p>: ""}
+            </div>
+            <input required value={value} onChange={handleChange} className={`${isSubmitted && value === "" ? "border-strawberry-red" :"border-light-gray"} p-3 border-solid border-light-gray border-[1px] rounded-xl active:border-marine-blue text-marine-blue font-bold placeholder:font-normal cursor-pointer`} placeholder={placeholder}/>
         </div>
     );
 }
--- multi-step-form/app/_components/steps/StepOne.tsx
+++ multi-step-form/app/_components/steps/StepOne.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {FormEventHandler, useState} from 'react';
 import Input from "@/app/_components/Input";
 import Button from "@/app/_components/Button";
 
 function StepOne({setStepNumber}: {setStepNumber: React.Dispatch<React.SetStateAction<number>>}) {
     const increaseStep: React.MouseEventHandler<HTMLButtonElement> = ()=>{
-        setStepNumber((s)=>s+1)
+        if(name!=="" && email!=="" && phoneNumber!=="") setStepNumber((s)=>s+1)
+    }
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+
+    const [isSubmitted, setIsSubmitted] = useState(false)
+
+    const handleSubmit: FormEventHandler<HTMLFormElement> = function(e){
+        e.preventDefault();
+        setIsSubmitted((s)=>!s)
     }
 
     return (
-        <form className="flex flex-col h-full justify-between">
+        <form onSubmit={handleSubmit} className="flex flex-col h-full justify-between" noValidate>
             <div>
-            <Input label="Name" placeholder="e.g. Stephen King"/>
-            <Input label="Email Address" placeholder="e.g. stephenking@lorem.com"/>
-            <Input label="Phone Number" placeholder="e.g. +1 234 567 890"/>
+            <Input value={name} onChange={setName} isSubmitted={isSubmitted} label="Name" placeholder="e.g. Stephen King"/>
+            <Input value={email} onChange={setEmail} isSubmitted={isSubmitted} label="Email Address" placeholder="e.g. stephenking@lorem.com"/>
+            <Input value={phoneNumber} onChange={setPhoneNumber} isSubmitted={isSubmitted} label="Phone Number" placeholder="e.g. +1 234 567 890"/>
             </div>
             <div className="self-end">
                 <Button setStepNumber={increaseStep} content="Next Step"/>
