4cc9895176d8fb11a2e559dd54b7d4352c6b32ab
kacperjanus <kacperjanus8@gmail.com>
2024-05-26 11:04:29 +0200

    Added styling for active and hover states

--- multi-step-form/app/_components/Button.tsx
+++ multi-step-form/app/_components/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Button({content, confirm, setStepNumber, backButton} : {content: string, confirm?: boolean, setStepNumber: React.MouseEventHandler<HTMLButtonElement>, backButton?: boolean}) {
 
     return (
-        backButton ? <button onClick={setStepNumber} className="text-cool-gray">{content}</button> :
+        backButton ? <button onClick={setStepNumber} className="text-cool-gray hover:text-marine-blue">{content}</button> :
         <button onClick={setStepNumber} className={`px-6 py-4 w-[7.56em] bg-marine-blue text-white rounded-lg ${confirm ? "bg-purplish-blue" : 'bg-marine-blue'}`}>{content}</button>
     );
 }
--- multi-step-form/app/_components/Input.tsx
+++ multi-step-form/app/_components/Input.tsx
@@ -4,7 +4,7 @@ function Input({label, placeholder}: {label: string; placeholder: string}) {
     return (
         <div className="flex flex-col pt-5">
             <label className="text-marine-blue">{label}</label>
-            <input required className="p-3 border-solid border-light-gray border-[1px] rounded-xl" placeholder={placeholder}/>
+            <input required className="p-3 border-solid border-light-gray border-[1px] rounded-xl active:border-marine-blue text-marine-blue font-bold placeholder:font-normal cursor-pointer" placeholder={placeholder}/>
         </div>
     );
 }
