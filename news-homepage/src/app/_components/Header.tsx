75e967b5ea23686dcbe240a74f2756dd5cff83ff
kacperjanus <kacperjanus8@gmail.com>
2024-05-29 15:58:14 +0200

    Implemented mobile navigation

--- news-homepage/src/app/_components/Header.tsx
+++ news-homepage/src/app/_components/Header.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+"use client"
+import React, {useState} from 'react';
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
 function Header() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
     return (
-        <div className="max-w-[68rem] mx-auto pt-20 pb-16 flex justify-between items-center bg-off-white px-4">
+        <div
+            className="max-w-[68rem] mx-auto pt-10 md:pt-20 pb-16 flex justify-between items-center bg-off-white px-4">
             <Logo/>
-            <Navigation/>
+            <Navigation isNavOpen={isNavOpen}/>
+            <p onClick={() => setIsNavOpen(s => !s)} className="fixed right-5 md:hidden z-20 text-5xl">{isNavOpen ?
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="size-16">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12"/>
+                </svg>
+                : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                       stroke="currentColor" className="size-16">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                </svg>
+            }</p>
         </div>
     );
 }
--- news-homepage/src/app/_components/Navigation.tsx
+++ news-homepage/src/app/_components/Navigation.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+"use client"
+
+import React, {useState} from 'react';
 import Link from "next/link";
 
-function Navigation() {
+function Navigation({isNavOpen} : {isNavOpen: boolean}) {
     return (
         <div>
-            <ul className="flex text-lg gap-10 text-dark-grayish-blue">
+            <ul className="hidden md:flex text-lg gap-10 text-dark-grayish-blue">
+                <li className="hover:text-soft-orange"><Link href="/">Home</Link></li>
+                <li className="hover:text-soft-orange"><Link href={'/'}>New</Link></li>
+                <li className="hover:text-soft-orange"><Link href={'/'}>Popular</Link></li>
+                <li className="hover:text-soft-orange"><Link href={'/'}>Trending</Link></li>
+                <li className="hover:text-soft-orange"><Link href={'/'}>Categories</Link></li>
+            </ul>
+            <div className={`transition-all bg-black h-full top-0 right-0 left-0 z-10 md:hidden opacity-30 ${isNavOpen ? "fixed" : "hidden"}`}></div>
+            <ul className={`transition-all z-10 bg-white h-screen ${isNavOpen ? "flex" : "translate-x-[20rem] flex"} fixed flex-col md:hidden text-lg gap-6 top-0 right-0 pt-48 w-[17rem] p-10 font-medium text-very-dark-blue`}>
                 <li className="hover:text-soft-orange"><Link href="/">Home</Link></li>
                 <li className="hover:text-soft-orange"><Link href={'/'}>New</Link></li>
                 <li className="hover:text-soft-orange"><Link href={'/'}>Popular</Link></li>
