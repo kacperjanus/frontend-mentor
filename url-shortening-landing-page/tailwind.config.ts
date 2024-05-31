218fe5e5dbf77dbd431e260018d28297ac593044
kacperjanus <kacperjanus8@gmail.com>
2024-05-31 17:57:34 +0200

    First commit for URL shortening landing page challenge

---
+++ url-shortening-landing-page/tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "cyan": "hsl(180, 66%, 49%)",
+        "dark-violet": "hsl(257, 27%, 26%)",
+        "red": "hsl(0, 87%, 67%)",
+        "gray": "hsl(0, 0%, 75%)",
+        "grayish-blue": "hsl(257, 7%, 63%)",
+        "very-dark-blue": "hsl(255, 11%, 22%)",
+        "very-dark-violet": "hsl(260, 8%, 14%)",
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
