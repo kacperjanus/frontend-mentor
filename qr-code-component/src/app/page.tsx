588fefe89108cae27576614ce2f5b749d7cb1020
kacperjanus <kacperjanus8@gmail.com>
2024-05-31 17:15:28 +0200

    Fixed styling, added font and official colors

--- qr-code-component/src/app/page.tsx
+++ qr-code-component/src/app/page.tsx
@@ -1,14 +1,16 @@
-import Image from "next/image";
 import QrCodeComponent from "@/components/QrCodeComponent";
+import {Outfit} from "next/font/google";
+
+const outfit = Outfit({subsets: ['latin'], weight: ['400', '700']});
 
 export default function Home() {
+
   const CodePath = "/image-qr-code.png";
   const CodeTitle = "Improve your front-end skills by building projects"
   const CodeDescription = "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
 
   return (
-      <div className="bg-slate-300 p-4 w-min">
-
+      <div className={`${outfit.className} bg-light-gray p-4 w-full h-screen flex justify-center items-center`}>
         <QrCodeComponent CodePath={CodePath} CodeTitle={CodeTitle} CodeDescription={CodeDescription}/>
       </div>
   );
--- qr-code-component/src/components/QrCodeComponent.tsx
+++ qr-code-component/src/components/QrCodeComponent.tsx
@@ -5,11 +5,11 @@ interface QrCodeComponentProps {
 }
 
 function QrCodeComponent({ CodePath, CodeTitle, CodeDescription }: QrCodeComponentProps) {
-    return <div className="rounded-2xl grid m-4 w-[320px] p-[16px] bg-white gap-y-[24px] border drop-shadow-xl">
+    return <div className="rounded-2xl grid m-4 w-[20rem] p-[1rem] bg-white gap-y-[1.5rem] border drop-shadow-xl">
         <img className="rounded-2xl size-[288px]" src={CodePath} alt="QR Code" />
-        <div className="grid gap-y-[16px]">
-            <div className="text-[22px] font-bold text-center">{CodeTitle}</div>
-            <div className="text-[15px] text-slate-400 text-center">{CodeDescription}</div>
+        <div className="grid gap-y-[1rem] pb-6">
+            <div className="text-[22px] font-[700] text-center">{CodeTitle}</div>
+            <div className="text-[14px] tracking-wider leading-5 font-[400] text-grayish-blue text-center">{CodeDescription}</div>
         </div>
     </div>
 }
