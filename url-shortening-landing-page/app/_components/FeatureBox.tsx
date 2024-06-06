76019c86b936ba84509546792900189fa931529e
kacperjanus <kacperjanus8@gmail.com>
2024-06-06 18:34:40 +0200

    Fixed icon padding

--- url-shortening-landing-page/app/_components/FeatureBox.tsx
+++ url-shortening-landing-page/app/_components/FeatureBox.tsx
@@ -9,7 +9,9 @@ interface Feature {
 function FeatureBox({feature, index} : {feature: Feature, index: number}) {
     return (
         <div className={`bg-white text-left pt-16 px-8 pb-8 relative z-10 ${index === 1 ? "mt-12" : ""} ${index === 2 ? "mt-24" : ""}`}>
-            <img className="absolute top-[-2rem] bg-dark-violet p-4 rounded-full" src={feature.image}/>
+            <div className="absolute top-[-2rem] bg-dark-violet p-4 rounded-full">
+                <img src={feature.image} alt="Feature icon"/>
+            </div>
             <h2 className="font-bold mb-4">{feature.title}</h2>
             <p className="text-gray text-sm">{feature.description}</p>
         </div>
