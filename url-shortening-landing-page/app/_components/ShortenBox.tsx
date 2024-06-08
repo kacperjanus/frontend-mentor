4e1e36f1f708b4cf2d0a3fb6565bbe23b95fc338
kacperjanus <kacperjanus8@gmail.com>
2024-06-08 09:43:27 +0200

    Added error state to ShortenBox

--- url-shortening-landing-page/app/_components/ShortenBox.tsx
+++ url-shortening-landing-page/app/_components/ShortenBox.tsx
@@ -9,7 +9,7 @@ interface TinyUrlResponse {
     "deleted": boolean,
     "archived": boolean,
     "tags": string[],
-    analytics: { "enabled": boolean, "public": boolean }[],
+    "analytics": { "enabled": boolean, "public": boolean }[],
     "tiny_url": string,
     "created_at": string,
     "expires_at": string | null,
@@ -18,22 +18,21 @@ interface TinyUrlResponse {
 
 function ShortenBox() {
     const [link, setLink] = useState("")
+    const [error, setError] = useState(true)
 
     async function handleClick() {
-        if(!link) return;
+        if(!link) {
+            setError(true);
+            return;
+        }
+        if(error) setError(false)
         const data: TinyUrlResponse = await ShortenUrl(link)
         if(data.tiny_url && data.url){
             if(localStorage.getItem("links")){
                 const links = JSON.parse(localStorage.getItem("links")!);
-                if(links.constructor === Array){
-                    const newValue = JSON.stringify([{url: data.url, tiny_url: data.tiny_url}, ...links]);
-                    localStorage.setItem("links", newValue)
-                    window.dispatchEvent(new StorageEvent("storage", { key: "links", newValue }));
-                }else{
-                    const newValue = JSON.stringify([{url: data.url, tiny_url: data.tiny_url}, links])
-                    localStorage.setItem("links", newValue)
-                    window.dispatchEvent(new StorageEvent("storage", { key: "links", newValue}));
-                }
+                const newValue = links.constructor === Array ? JSON.stringify([{url: data.url, tiny_url: data.tiny_url}, ...links]) : JSON.stringify([{url: data.url, tiny_url: data.tiny_url}, links]);
+                localStorage.setItem("links", newValue)
+                window.dispatchEvent(new StorageEvent("storage", { key: "links", newValue}));
             }else{
                 localStorage.setItem("links", JSON.stringify({url: data.url, tiny_url: data.tiny_url}))
             }
@@ -41,11 +40,15 @@ function ShortenBox() {
     }
 
     return (
-        <div className="absolute bottom-[-3.5rem] md:bottom-[-5.5rem] -right-3 -left-3 md:right-0 md:left-0">
-                    <div className="absolute md:w-[45rem] lg:w-[60rem] md:left-[50%] md:translate-x-[-50%] right-1 left-1 flex md:flex-row flex-col gap-4 md:gap-6 bg-shorten-desktop bg-cover p-4 bg-dark-violet rounded-lg ">
-                        <input className="px-4 w-full py-3 rounded-lg outline-none"
-                               placeholder="Shorten a link here..." value={link}
-                               onChange={(e) => setLink(e.target.value)}/>
+        <div className="absolute bottom-[-2rem] md:bottom-[-5rem] -right-3 -left-3 md:right-0 md:left-0">
+                    <div className="absolute md:w-[45rem] lg:w-[60rem] md:left-[50%] md:translate-x-[-50%] right-1 left-1 flex md:flex-row flex-col gap-8 md:gap-6 bg-shorten-desktop bg-cover p-8 md:p-6 bg-dark-violet rounded-lg ">
+                        <div className="w-full relative">
+                            <input className={`px-4 w-full py-3 rounded-lg outline-none box-border ${error ? "border-red border-2": "border-transparent border-2"}`}
+                                   placeholder="Shorten a link here..." value={link}
+                                   onChange={(e) => setLink(e.target.value)}/>
+                            {error && <span
+                                className="text-red text-sm italic absolute left-0 -bottom-5">Please add a link</span>}
+                        </div>
                         <button onClick={handleClick}
                                 className="bg-cyan w-full text-white rounded-md px-4 py-2 md:w-40 hover:bg-[#9AE2E2]">Shorten it!
                         </button>
