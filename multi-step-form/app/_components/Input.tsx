b251c01fe61498928eac1263ac309a858623b5b0
kacperjanus <kacperjanus8@gmail.com>
2024-06-09 12:19:30 +0200

    Implemented field validation with zod and validator.js

--- multi-step-form/app/_components/Input.tsx
+++ multi-step-form/app/_components/Input.tsx
@@ -1,7 +1,6 @@
 import React, {ChangeEventHandler} from 'react';
-import ansiRegex from "ansi-regex";
 
-function Input({label, placeholder, value, onChange, isSubmitted}: {label: string; placeholder: string, value: string, onChange: React.Dispatch<React.SetStateAction<string>>, isSubmitted: boolean}) {
+function Input({label, placeholder, value, onChange, isSubmitted, zodParse}: {label: string; placeholder: string, value: string, onChange: React.Dispatch<React.SetStateAction<string>>, isSubmitted: boolean, zodParse: boolean}) {
     const handleChange: ChangeEventHandler<HTMLInputElement> = function(e){
         onChange(e?.target.value)
     }
@@ -10,7 +9,7 @@ function Input({label, placeholder, value, onChange, isSubmitted}: {label: strin
         <div className="flex flex-col pt-5">
             <div className="flex justify-between items-center">
                 <label className="text-marine-blue">{label}</label>
-                {isSubmitted && value==="" ? <p className="font-bold text-strawberry-red"> This field is required</p>: ""}
+                {isSubmitted && value==="" ? <p className="font-bold text-strawberry-red"> This field is required</p>: isSubmitted && !zodParse ? <p className="font-bold text-strawberry-red"> Incorrect format</p>: ""}
             </div>
             <input required value={value} onChange={handleChange} className={`${isSubmitted && value === "" ? "border-strawberry-red" :"border-light-gray"} p-3 border-solid border-[1px] rounded-xl active:border-marine-blue text-marine-blue font-bold placeholder:font-normal cursor-pointer`} placeholder={placeholder}/>
         </div>
--- multi-step-form/app/_components/steps/StepOne.tsx
+++ multi-step-form/app/_components/steps/StepOne.tsx
@@ -1,6 +1,8 @@
 import React, {FormEventHandler, useState} from 'react';
 import Input from "@/app/_components/Input";
 import Button from "@/app/_components/Button";
+import {z} from "zod";
+import validator from "validator";
 
 interface StepOneProps {
     setStepNumber: React.Dispatch<React.SetStateAction<number>>;
@@ -14,7 +16,9 @@ interface StepOneProps {
 
 function StepOne({setStepNumber, email, name, phoneNumber, setName, setPhoneNumber, setEmail}: StepOneProps) {
     const increaseStep: React.MouseEventHandler<HTMLButtonElement> = ()=>{
-        if(name!=="" && email!=="" && phoneNumber!=="") setStepNumber((s)=>s+1)
+        if(nameParse && emailParse && phoneNumberParse){
+            setStepNumber((s)=>s+1)
+        }
     }
 
     const [isSubmitted, setIsSubmitted] = useState(false)
@@ -24,12 +28,20 @@ function StepOne({setStepNumber, email, name, phoneNumber, setName, setPhoneNumb
         setIsSubmitted(true)
     }
 
+    const emailSchema = z.string().email();
+    const phoneNumberSchema = z.string().refine(validator.isMobilePhone);
+    const nameSchema = z.string();
+
+    const nameParse = nameSchema.safeParse(name).success;
+    const emailParse = emailSchema.safeParse(email).success;
+    const phoneNumberParse = phoneNumberSchema.safeParse(phoneNumber).success;
+
     return (
         <form onSubmit={handleSubmit} className="flex flex-col h-full justify-between" noValidate>
             <div>
-            <Input value={name} onChange={setName} isSubmitted={isSubmitted} label="Name" placeholder="e.g. Stephen King"/>
-            <Input value={email} onChange={setEmail} isSubmitted={isSubmitted} label="Email Address" placeholder="e.g. stephenking@lorem.com"/>
-            <Input value={phoneNumber} onChange={setPhoneNumber} isSubmitted={isSubmitted} label="Phone Number" placeholder="e.g. +1 234 567 890"/>
+            <Input zodParse={nameParse} value={name} onChange={setName} isSubmitted={isSubmitted} label="Name" placeholder="e.g. Stephen King"/>
+            <Input zodParse={emailParse} value={email} onChange={setEmail} isSubmitted={isSubmitted} label="Email Address" placeholder="e.g. stephenking@lorem.com"/>
+            <Input zodParse={phoneNumberParse} value={phoneNumber} onChange={setPhoneNumber} isSubmitted={isSubmitted} label="Phone Number" placeholder="e.g. +1 234 567 890"/>
             </div>
             <div className="md:static w-full fixed bg-white top-[31em] bottom-0 right-0 justify-center md:justify-end flex py-4 pr-4 md:py-0 md:pr-0 self-end md:bg-none ">
                 <div className="ml-[10.7rem] md:ml-0">
