70628716b0d25c26e37bc36589be07cc6e1fcd21
kacperjanus <kacperjanus8@gmail.com>
2024-06-13 16:22:10 +0200

    Style fix

--- space-tourism-website/app/_components/single-components/NavigationItem.tsx
+++ space-tourism-website/app/_components/single-components/NavigationItem.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 function NavigationItem({number, name, active, to}: {number: number, name: string, active: number, to: string}) {
     return (
-        <div className={`my-auto nav-text h-full py-10 border-b-2 border-transparent hover:border-b-[rgba(255,255,255,0.5)] cursor-pointer ${number === active ? "border-b-white hover:border-b-white border-b-2" : ""}`}>
             <Link href={to}>
+        <div className={`my-auto nav-text h-full py-10 border-b-2 border-transparent hover:border-b-[rgba(255,255,255,0.5)] cursor-pointer ${number === active ? "border-b-white hover:border-b-white border-b-2" : ""}`}>
             <span>{number < 10 ? `0${number}` : "number"}</span>
             <span> </span>
             <span className="uppercase">{name}</span>
-            </Link>
         </div>
+            </Link>
     );
 }
 
