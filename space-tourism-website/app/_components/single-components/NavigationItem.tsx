97617eacf07d54dad53a578ad63bba9dccd93aa6
kacperjanus <kacperjanus8@gmail.com>
2024-06-15 17:43:29 +0200

    Added transitions

--- space-tourism-website/app/_components/single-components/NavigationItem.tsx
+++ space-tourism-website/app/_components/single-components/NavigationItem.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function NavigationItem({number, name, active, to}: {number: number, name: string, active: boolean, to: string}) {
     return (
             <Link href={to}>
-        <div className={`my-auto nav-text h-full py-10 border-b-2 border-transparent hover:border-b-[rgba(255,255,255,0.5)] cursor-pointer ${active ? "border-b-white hover:border-b-white border-b-2" : ""}`}>
+        <div className={`my-auto nav-text h-full py-10 border-b-2 border-transparent hover:border-b-[rgba(255,255,255,0.5)] cursor-pointer transition-all duration-200 ${active ? "border-b-white hover:border-b-white border-b-2" : ""}`}>
             <span>{number < 10 ? `0${number}` : "number"}</span>
             <span> </span>
             <span className="uppercase">{name}</span>
--- space-tourism-website/app/_components/single-components/SmallPagination.tsx
+++ space-tourism-website/app/_components/single-components/SmallPagination.tsx
@@ -4,7 +4,7 @@ type CrewMember  = "mark-shuttleworth" | "victor-glover" | "douglas-hurley" | "a
 
 function SmallPagination({active, onClick}: {active: boolean, onClick: ()=>void}) {
     return (
-        <button onClick={onClick} className={`h-[10px] w-[10px] desktop:h-[15px] desktop:w-[15px] rounded-full ${active ? "bg-white" : "bg-[#36373F] hover:bg-grey"}`}></button>
+        <button onClick={onClick} className={`h-[10px] w-[10px] desktop:h-[15px] desktop:w-[15px] rounded-full transition-all duration-200 ${active ? "bg-white" : "bg-[#36373F] hover:bg-grey"}`}></button>
     );
 }
 
