1d08dcd213fedd0495c269d6c54f1b84f73265ac
kacperjanus <kacperjanus8@gmail.com>
2024-06-15 16:55:30 +0200

    Fixed mobile navigation icon size

--- url-shortening-landing-page/app/_components/Header.tsx
+++ url-shortening-landing-page/app/_components/Header.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 function Header() {
     const [navOpen, setNavOpen] = useState(false);
     return (
-        <div className="bg-white w-screen h-24 px-10 flex items-center gap-10 max-w-7xl mx-auto pt-10 justify-between md:justify-normal">
+        <div className="bg-white w-screen h-24 px-10 flex items-start md:items-center gap-10 max-w-7xl mx-auto pt-10 justify-between md:justify-normal">
             <Logo/>
             <div className="w-full hidden md:flex md:items-center justify-between">
                 <div className="flex gap-6">
@@ -20,9 +20,21 @@ function Header() {
                     <Link className="text-white text-sm font-medium bg-cyan px-6 py-2 rounded-full hover:bg-[#9AE2E2]" href="/">Sign Up</Link>
                 </div>
             </div>
-            {!navOpen && <button onClick={()=>setNavOpen(s=>!s)} className="md:hidden text-5xl">&#9776;</button>}
-            {navOpen && <button onClick={()=>setNavOpen(s=>!s)} className="md:hidden self-end text-5xl">&times;</button>}
-            {navOpen && <div className="fixed z-20 bg-dark-violet top-32 right-10 left-10 rounded-lg text-white py-10 divide-y-[1px] divide-grayish-blue px-2">
+            {!navOpen && <button onClick={() => setNavOpen(s => !s)} className="md:hidden -mt-1">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" className="size-8">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                </svg>
+            </button>}
+            {navOpen && <button onClick={() => setNavOpen(s => !s)} className="md:hidden -mt-1">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" className="size-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
+                </svg>
+            </button>}
+            {navOpen && <div
+                className="fixed md:hidden z-20 bg-dark-violet top-32 right-10 left-10 rounded-lg text-white py-10 divide-y-[1px] divide-grayish-blue px-2">
                 <div className="flex flex-col gap-6 text-lg w-full items-center mb-6">
                     <Link className="font-bold"
                           href="/">Features</Link>
