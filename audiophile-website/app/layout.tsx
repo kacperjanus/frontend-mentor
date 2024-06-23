8c317338f7209c32eedb680ae95f8860b8ec3405
kacperjanus <kacperjanus8@gmail.com>
2024-06-23 18:21:59 +0200

    Commiting starter project files for Audiophile Website

---
+++ audiophile-website/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+const manrope = Manrope({ subsets: ["latin"],  });
+
+export const metadata: Metadata = {
+  title: "Audiophile Website",
+  description: "Frontend Mentor challenge solution",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={manrope.className}>{children}</body>
+    </html>
+  );
+}
