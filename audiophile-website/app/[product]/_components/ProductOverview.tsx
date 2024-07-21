65cf27a8e782ad8ec350810365fd01e68f80dfc5
kacperjanus <kacperjanus8@gmail.com>
2024-07-21 23:37:16 +0200

    Implemented tablet version of product page

--- audiophile-website/app/[product]/_components/ProductOverview.tsx
+++ audiophile-website/app/[product]/_components/ProductOverview.tsx
@@ -3,15 +3,20 @@ import Button from "@/app/_components/Button";
 import React from "react";
 
 function ProductOverview({productData}: { productData: Product }) {
-    return <div className="flex flex-col gap-8 mb-16">
-        <img className="rounded-lg" src={productData.image.mobile} alt={productData.name}/>
-        {productData.new && <span className="overline-element uppercase">new product</span>}
-        <h2 className="heading-4">{productData.name}</h2>
-        <p className="text-dark-grey text-[15px] leading-[25px]">{productData.description}</p>
-        <span className="font-bold text-[18px] tracking-[1.29px]">{`$ ${productData.price.toLocaleString()}`}</span>
-        <div className="flex gap-4">
-            <Numbers max={10}/>
-            <Button type="primary">Add to cart</Button>
+    return <div className="flex flex-col tablet:grid tablet:grid-cols-[2fr_3fr] tablet:gap-16 gap-8 mb-16">
+        <img className="rounded-lg block tablet:hidden" src={productData.image.mobile} alt={productData.name}/>
+        <img className="rounded-lg hidden tablet:block desktop:hidden" src={productData.image.tablet}
+             alt={productData.name}/>
+        <img className="rounded-lg hidden desktop:block" src={productData.image.desktop} alt={productData.name}/>
+        <div className="flex flex-col gap-6 tablet:justify-center">
+            {productData.new && <span className="overline-element uppercase">new product</span>}
+            <h2 className="heading-4">{productData.name}</h2>
+            <p className="text-dark-grey text-[15px] leading-[25px]">{productData.description}</p>
+            <span className="font-bold text-[18px] tracking-[1.29px]">{`$ ${productData.price.toLocaleString()}`}</span>
+            <div className="flex gap-4">
+                <Numbers max={10}/>
+                <Button type="primary">Add to cart</Button>
+            </div>
         </div>
     </div>;
 }
--- audiophile-website/app/[product]/_components/YouMayAlsoLike.tsx
+++ audiophile-website/app/[product]/_components/YouMayAlsoLike.tsx
@@ -3,13 +3,18 @@ import Button from "@/app/_components/Button";
 
 function YouMayAlsoLike({productData}: { productData: Product }) {
     return (
-        <div className="flex flex-col gap-12">
+        <div className="flex flex-col gap-12 mb-12">
             <h3 className="heading-5 uppercase text-center">you may also like</h3>
-            {productData.others.map((item) => <div className="flex flex-col items-center gap-8">
-                <img className="rounded-lg" src={item.image.mobile} alt={item.name}/>
-                <h4 className="heading-5">{item.name}</h4>
-                <Button type="primary">See product</Button>
-            </div>)}
+            <div className="flex flex-col tablet:flex-row gap-12">
+                {productData.others.map((item) => <div className="flex flex-col items-center gap-8">
+                    <img className="block tablet:hidden rounded-lg" src={item.image.mobile} alt={item.name}/>
+                    <img className="hidden tablet:block desktop:hidden rounded-lg" src={item.image.tablet}
+                         alt={item.name}/>
+                    <img className="hidden desktop:block rounded-lg" src={item.image.desktop} alt={item.name}/>
+                    <h4 className="heading-5">{item.name}</h4>
+                    <Button type="primary">See product</Button>
+                </div>)}
+            </div>
         </div>
     );
 }
