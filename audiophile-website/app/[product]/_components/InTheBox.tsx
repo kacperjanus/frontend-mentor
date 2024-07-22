0560699809314727ca0c31a72b90687514e54945
kacperjanus <kacperjanus8@gmail.com>
2024-07-22 19:39:15 +0200

    Bug fix

--- audiophile-website/app/[product]/_components/InTheBox.tsx
+++ audiophile-website/app/[product]/_components/InTheBox.tsx
@@ -5,7 +5,7 @@ function InTheBox({productData}: { productData: Product }) {
         <div className="flex flex-col tablet:grid tablet:grid-cols-2 desktop:flex desktop:flex-col gap-8 mb-16">
             <h3 className="heading-5 desktop:heading-3 uppercase">in the box</h3>
             <ul>
-                {productData.includes.map((item, index) => <li className="grid grid-cols-[1fr_7fr] mb-2">
+                {productData.includes.map((item, index) => <li key={index} className="grid grid-cols-[1fr_7fr] mb-2">
                     <span className="text-earthy font-bold">{item.quantity}x </span>
                     <span className="font-medium text-dark-grey text-[15px]">{item.item}</span>
                 </li>)}
--- audiophile-website/app/_components/MobileNavigationSection.tsx
+++ audiophile-website/app/_components/MobileNavigationSection.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from "@/app/_components/Button";
-import Link from "next/link";
 
 interface MobileNavigationSectionProps {
     title: string
@@ -10,13 +9,12 @@ interface MobileNavigationSectionProps {
 
 function MobileNavigationSection({title, image, to}: MobileNavigationSectionProps) {
     return (
-        <Link href={to}>
-            <div className="mt-16 w-[327px] h-[165px] tablet:w-[223px] desktop:w-[350px] bg-grey rounded-lg flex flex-col justify-end items-center gap-3 p-4">
-                <img className="h-[100%]" src={image} alt="Category thumbnail"/>
-                <span className="text-[15px] uppercase tracking-[1.07px]">{title}</span>
-                <Button type="tertiary">shop</Button>
-            </div>
-        </Link>
+        <div
+            className="mt-16 w-[327px] h-[165px] tablet:w-[223px] desktop:w-[350px] bg-grey rounded-lg flex flex-col justify-end items-center gap-3 p-4">
+            <img className="h-[100%]" src={image} alt="Category thumbnail"/>
+            <span className="text-[15px] uppercase tracking-[1.07px]">{title}</span>
+            <Button link={to} type="tertiary">shop</Button>
+        </div>
     );
 }
 
