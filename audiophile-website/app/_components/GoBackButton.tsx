f5da1e5f6c01633047175177b10c102ae7cbbffb
kacperjanus <kacperjanus8@gmail.com>
2024-07-22 14:09:30 +0200

    Style fix

--- audiophile-website/app/_components/GoBackButton.tsx
+++ audiophile-website/app/_components/GoBackButton.tsx
@@ -6,7 +6,7 @@ import {useRouter} from "next/navigation";
 function GoBackButton() {
     const router = useRouter()
     return (
-        <div className="my-4 desktop:mt-16 desktop:mb-12">
+        <div className="py-4 desktop:pt-16 desktop:pb-12">
             <button onClick={() => router.back()}>
                 <span className="text-[15px] font-medium leading-[25px] text-dark-grey">Go Back</span>
             </button>
--- audiophile-website/app/_components/TextField.tsx
+++ audiophile-website/app/_components/TextField.tsx
@@ -8,12 +8,15 @@ interface TextFieldProps {
 
 function TextField({label, placeholder, error}: TextFieldProps) {
     return (
-        <div className="w-[309px] flex flex-col gap-2">
+        <div className="w-full flex flex-col gap-2 mb-6">
             <div className="flex justify-between items-center">
-                <label className={`${error ? "text-red-700" : ""} text-[12px] font-bold`} htmlFor={label}>{label}</label>
+                <label className={`${error ? "text-red-700" : ""} text-[12px] font-bold`}
+                       htmlFor={label}>{label}</label>
                 {error && <span className="text-red-700 text-[12px]">Wrong format</span>}
             </div>
-            <input className={`border-solid border-[1px] w-full font-bold border-[#CFCFCF] rounded-lg py-4 px-6 placeholder:font-medium placeholder:text-[14px] focus:border-earthy focus:outline-none ${error ? "border-[2px] border-red-700" : ""}`} type="text" placeholder={placeholder} />
+            <input
+                className={`border-solid border-[1px] w-full font-bold border-[#CFCFCF] rounded-lg py-4 px-6 placeholder:font-medium placeholder:text-[14px] focus:border-earthy focus:outline-none ${error ? "border-[2px] border-red-700" : ""}`}
+                type="text" placeholder={placeholder}/>
         </div>
     );
 }
