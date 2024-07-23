6639660f9e211dd0ce0888ebd299029457e8319b
kacperjanus <kacperjanus8@gmail.com>
2024-07-23 15:44:30 +0200

    Added custom className prop

--- audiophile-website/app/_components/Overlay.tsx
+++ audiophile-website/app/_components/Overlay.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Overlay({onClick}: { onClick?: () => void }) {
+function Overlay({onClick, className}: { onClick?: () => void, className?: string }) {
     return (
         <div onClick={onClick}
-             className="fixed top-0 bottom-0 right-0 left-0 bg-[rgba(0,0,0,0.5)]"></div>
+             className={`fixed top-0 bottom-0 right-0 left-0 bg-[rgba(0,0,0,0.5)] ${className}`}></div>
     );
 }
 
