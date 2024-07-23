d89b0d312849e9f5921ce2c2a295e086ad3ad340
kacperjanus <kacperjanus8@gmail.com>
2024-07-23 13:40:36 +0200

    Added label to RadioSelectGroup component

--- audiophile-website/app/_components/RadioSelectGroup.tsx
+++ audiophile-website/app/_components/RadioSelectGroup.tsx
@@ -3,11 +3,17 @@
 import React, {useState} from 'react';
 import RadioSelect from "@/app/_components/RadioSelect";
 
-function RadioSelectGroup({labels}: { labels: string[] }) {
+function RadioSelectGroup({labels, mainLabel}: { labels: string[], mainLabel: string }) {
     const [checked, setChecked] = useState(0)
 
-    return labels.map((label, i) => <RadioSelect setChecked={setChecked} index={i} label={label} active={checked === i}
-                                                 key={i}/>)
+    return <div className="flex flex-col tablet:grid tablet:grid-cols-2">
+        <p className={`text-[12px] font-bold mb-2`}>{mainLabel}</p>
+        <div>
+            {labels.map((label, i) => <RadioSelect setChecked={setChecked} index={i} label={label}
+                                                   active={checked === i}
+                                                   key={i}/>)}
+        </div>
+    </div>
 }
 
 export default RadioSelectGroup;
