20b32bb090d8c08a047b64ffe87964faff0164a8
kacperjanus <kacperjanus8@gmail.com>
2024-07-23 15:27:37 +0200

    Refactored Button component to handle both link and function

--- audiophile-website/app/_components/Button.tsx
+++ audiophile-website/app/_components/Button.tsx
@@ -1,78 +1,35 @@
-import React, {MouseEventHandler, ReactNode} from 'react';
-import Link from "next/link";
+"use client"
+
+import React, {ReactNode} from 'react';
+import {useRouter} from "next/navigation";
 
 function Button({type, children, link, fn, className}: {
     type: string,
     children?: ReactNode,
     link?: string,
-    fn?: MouseEventHandler<HTMLDivElement>
+    fn?: () => void
     className?: string
 }) {
-    function ButtonPrimary() {
-        return <button className={`w-40 h-12 bg-earthy hover:bg-earthy-light ${className}`}><span
-            className="uppercase text-[13px] font-bold text-white">{children}</span></button>
+    const router = useRouter();
+
+    function handleClick() {
+        fn && fn()
+        link && router.push(link)
     }
 
-    function ButtonSecondary() {
-        return <button
-            className={`w-40 h-12 bg-transparent border-[1px] border-solid border-black hover:bg-black group ${className}`}><span
-            className="uppercase text-[13px] group-hover:text-white font-bold text-black">{children}</span></button>
-    }
+    if (type === "primary") return <button onClick={handleClick}
+                                           className={`w-40 h-12 bg-earthy hover:bg-earthy-light ${className}`}><span
+        className="uppercase text-[13px] font-bold text-white">{children}</span></button>
 
-    function ButtonTertiary() {
-        return <button className={`w-[58px] h-[18px] group flex items-center gap-[13px] ${className}`}><span
-            className="uppercase text-[13px] font-bold text-[#808080] group-hover:text-earthy">{children}</span><span
-            className="inline-block"><img alt="Right arrow icon"
-                                          src="/assets/shared/desktop/icon-arrow-right.svg"/></span></button>
-    }
+    if (type === "secondary") return <button onClick={handleClick}
+                                             className={`w-40 h-12 bg-transparent border-[1px] border-solid border-black hover:bg-black group ${className}`}><span
+        className="uppercase text-[13px] group-hover:text-white font-bold text-black">{children}</span></button>
 
-    if (type === "primary") {
-        if (link) {
-            return <Link href={link || ""}>
-                <ButtonPrimary/>
-            </Link>
-        }
-
-        if (fn) {
-            return <div onClick={fn}>
-                <ButtonPrimary/>
-            </div>
-        }
-
-        return <ButtonPrimary/>;
-    }
-
-    if (type === "secondary") {
-        if (link) {
-            return <Link href={link || ""}>
-                <ButtonSecondary/>
-            </Link>
-        }
-
-        if (fn) {
-            return <div onClick={fn}>
-                <ButtonSecondary/>
-            </div>
-        }
-
-        return <ButtonSecondary/>
-    }
-
-    if (type === "tertiary") {
-        if (link) {
-            return <Link href={link || ""}>
-                <ButtonTertiary/>
-            </Link>
-        }
-
-        if (fn) {
-            return <div onClick={fn}>
-                <ButtonSecondary/>
-            </div>
-        }
-
-        return <ButtonTertiary/>
-    }
+    if (type === "tertiary") return <button onClick={handleClick}
+                                            className={`w-[58px] h-[18px] group flex items-center gap-[13px] ${className}`}><span
+        className="uppercase text-[13px] font-bold text-[#808080] group-hover:text-earthy">{children}</span><span
+        className="inline-block"><img alt="Right arrow icon"
+                                      src="/assets/shared/desktop/icon-arrow-right.svg"/></span></button>
 }
 
 export default Button;
