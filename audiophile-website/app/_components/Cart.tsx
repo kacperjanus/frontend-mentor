12a9cda0dbbc1796e7cc04c819a1ecbe7dae718c
kacperjanus <kacperjanus8@gmail.com>
2024-07-24 16:51:56 +0200

    Style fix

--- audiophile-website/app/_components/Cart.tsx
+++ audiophile-website/app/_components/Cart.tsx
@@ -10,11 +10,11 @@ function Cart() {
 
     return (
         <motion.div initial={{scale: 0}} animate={{scale: 1}}
-                    className="absolute rounded-lg z-20 tablet:w-[377px] bg-white top-5 right-5 tablet:right-10 desktop:right-40 left-5 tablet:left-auto p-8">
+                    className="absolute rounded-lg tablet:w-[377px] bg-white top-5 right-5 tablet:right-10 desktop:right-40 left-5 tablet:left-auto p-8">
             <div className="flex justify-between items-center">
                 <h1 className="heading-6">CART ({cart?.length})</h1>
                 {cart.length > 0 &&
-                    <span className="font-medium text-[15px] underline text-dark-grey" onClick={clearCart}>Remove all</span>}
+                    <span className="font-medium text-[15px] underline text-dark-grey cursor-pointer" onClick={clearCart}>Remove all</span>}
             </div>
 
             <CartItems cart={cart}/>
