ce2d7ae21749b5b47a0e58d91ff8eb40ed2047fb
kacperjanus <kacperjanus8@gmail.com>
2024-07-28 23:16:33 +0200

    Added project colors

--- devlinks/tailwind.config.ts
+++ devlinks/tailwind.config.ts
@@ -8,11 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        "dark-lavender": "#633CFF",
+        "lavender": "#BEADFF",
+        "light-lavender": "#EFEBFF",
+        "dark-gray": "#333",
+        "gray": "#737373",
+        "light-gray": "#D9D9D9",
+        "off-white": "#FAFAFA",
+        "white": "#FFF",
+        "tomato": "#FF3939",
+      }
     },
   },
   plugins: [],
