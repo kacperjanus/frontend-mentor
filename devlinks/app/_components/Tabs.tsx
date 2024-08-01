a89a05b9113a1c27c82e7c70dd3fb60c13f033ab
kacperjanus <kacperjanus8@gmail.com>
2024-08-01 23:08:52 +0200

    Added ability to customize tabs icons

--- devlinks/app/_components/Tabs.tsx
+++ devlinks/app/_components/Tabs.tsx
@@ -3,13 +3,14 @@
 import Image from "next/image";
 import {useState} from "react";
 
-export default function Tabs({labels}: {labels: string[]}) {
+export default function Tabs({labels, icons}: { labels: string[], icons: string[] }) {
     const [active, setActive] = useState(0)
 
-    return <div className="flex gap-4">
-        {labels.map((label, i) => <div key={label} onClick={()=>setActive(i)} className={`flex gap-2 px-[27px] py-[11px] rounded-lg heading-s hover:text-dark-lavender cursor-pointer ${active === i ? "bg-lavender text-dark-lavender" : "text-gray"}`}>
-        <Image width={20} height={20} className={`left-3 hover:stroke-dark-lavender ${active ? "stroke-dark-lavender" : ""}`} src="/assets/images/icon-link.svg" alt="Chain icon"/>
-            {label}
-        </div>)}
+    return <div className="flex">
+        {labels.map((label, i) =>
+            <div key={label} onClick={() => setActive(i)} className={`flex gap-2 px-[27px] py-[11px] rounded-lg heading-s hover:text-dark-lavender cursor-pointer ${active === i ? "bg-lavender text-dark-lavender" : "text-gray"}`}>
+                <Image width={20} height={20} className={`left-3 hover:stroke-dark-lavender ${active ? "stroke-dark-lavender" : ""}`} src={icons[i]} alt="Chain icon"/>
+                {label}
+            </div>)}
     </div>
 }
